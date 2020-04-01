4cb22169c66715c2705f74a030d7dc54e569cbf3
Marlon <marmoneymar@icloud.com>
2020-04-01 19:00:14 -0400

    (Added) Symbol configs

---
+++ Symbol+Config.swift
@@ -0,0 +1,14 @@
+//
+//  Symbol+Config.swift
+//  Neu Music Player
+//
+//  Created by Marlon Raskin on 4/1/20.
+//  Copyright © 2020 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage.SymbolConfiguration {
+	static let navButtonConfig = UIImage.SymbolConfiguration(pointSize: 12, weight: .bold)
+	static let playbackControlsConfig = UIImage.SymbolConfiguration(pointSize: 16, weight: .bold)
+}
